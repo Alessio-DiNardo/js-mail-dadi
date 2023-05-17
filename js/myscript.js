faa5ef7b42159a13e1044704a7a2a5a59f81da34
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-17 14:38:10 +0200

    try but no work for with if

--- js/myscript.js
+++ js/myscript.js
@@ -4,3 +4,17 @@
 // SE IL GIOCATORE HA UN NUMERO > DEL NUMERO DELLA CPU ALLORA HA VINTO
 // VICEVERSA SE HA UN NUMERO < DI QUELLO DELLA CPU ALLORA HA PERSO
 // SE ENTRAMBI AVRANNO NUMERO = ALLORA SARà PAREGGIO
+
+
+        let playerNumber = number;
+        let cpuNumber = number;
+
+for (let i = 0; i < 6; i++) {
+    number = Math.round(Math.random()*6 + 1);
+    } if (let playerNumber.value = Math.round(Math.random()*6 + 1) > let cpuNumber.value = Math.round(Math.random()*6 + 1));{
+        console.log("Hai vinto");
+    } if (let playerNumber.value = Math.round(Math.random()*6 + 1) < let cpuNumber.value = Math.round(Math.random()*6 + 1));{
+        console.log("Hai perso");
+    } else if (let playerNumber.value = Math.round(Math.random()*6 + 1) = let cpuNumber.value = Math.round(Math.random()*6 + 1));{
+        console.log("Hai pareggiato");
+    }   
